c6b5efe557fc5635dd6b5992569f4c091c43260b
Silviu <silviu_miron_90@outlook.com>
2025-07-10 16:56:25 +0100

    Update 10days.js

---
+++ July_2025/10days.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//  Problem: Day 0 Hello, World
+//  Link: https://www.hackerrank.com/challenges/js10-hello-world/problem?isFullScreen=true
+//  Date: 2025-07-10
+//  Language: JavaScript
+
+console.log('Hell0 World!');
