c8515bfddbf6aabf8d44c73a811062a38ff233d3
Silviu <silviu_miron_90@outlook.com>
2025-08-11 17:50:41 +0100

    Feat: Created onclick event, new helper functions,  and variables for form validation

--- August_2025/freecodecamp/rolePlayGame/rolePlayGame.js
+++ August_2025/freecodecamp/rolePlayGame/rolePlayGame.js
@@ -11,3 +11,41 @@ let inventory = ['stick'];
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
 const button3 = document.querySelector('#button3');
+
+const text = document.querySelector('#text');
+const xpText = document.querySelector('#xpText');
+const healtText = document.querySelector('#healtText');
+const goldText = document.querySelector('#goldText');
+const monsterStats = document.querySelector('#monsterStats');
+const monsterName = document.querySelector('monsterName');
+
+const monsterHealthText = document.querySelector('#monsterHealth');
+
+function goStore() {
+  button1.innerText = 'Buy 10 health (10 gold)';
+  button2.innerText = 'Buy weapon (30 gold)';
+  button3.innerText = 'Go to town square';
+
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+}
+goStore();
+
+function goCave() {
+  console.log('Going to cave.');
+}
+goCave();
+
+function fightDragon() {
+  console.log('Fighting dragon.');
+}
+fightingDragon();
+
+// Initialize buttons
+
+button1.onclick = goStore;
+
+button2.onclick = goCave;
+
+button3.onclick = fightDragon;
