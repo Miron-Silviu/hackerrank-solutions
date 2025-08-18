13fbb4939bf4874ad21f726ea6f791356e45deaa
Silviu <silviu_miron_90@outlook.com>
2025-08-18 11:32:09 +0100

    Create spred and rest operators file.

---
+++ August_2025/completeJsCourseAugust/spredAndRestOperatords.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Starting DAY
+// ============================================
+// 1
+//  Problem: Copying an Object (Spread):
+//  Date: 2025-08-12
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+const person = { name: 'Alica', age: 25 };
+
+//  🔹 Task
+
+// Make a new obect called `personalCopy `using the spread operator and log it.
