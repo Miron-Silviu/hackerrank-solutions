4319c479f5c2dad791373951a0ae66d2d101426c
Silviu <silviu_miron_90@outlook.com>
2025-08-24 13:21:17 +0100

    Sovlve loop challenges .

--- August_2025/completeJsCourseAugust/forOfLoop.js
+++ August_2025/completeJsCourseAugust/forOfLoop.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('test');
+// console.log('test');
 
 // ============================================
 // 1
@@ -9,6 +9,7 @@ console.log('test');
 //  Difficulty: Easy
 // ============================================
 
+console.log(`************ 1 **************`);
 let fruits = ['apple', 'banana', 'mango'];
 
 // TODO Use for loop to print each fruit
@@ -24,6 +25,8 @@ for (const fruit of fruits) {
 //  Language: JavaScript
 //  Difficulty: Easy
 // ============================================
+
+console.log(`************ 2 **************`);
 
 let numbers = [5, 10, 15, 20];
 
@@ -43,6 +46,8 @@ console.log(total);
 //  Language: JavaScript
 //  Difficulty: Easy
 // ============================================
+
+console.log(`************ 3 **************`);
 
 function getVowels(word) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -66,13 +71,81 @@ console.log(getVowels('o'));
 //  Difficulty: Medium
 // ============================================
 
-// let words = ['pen', 'javascript', 'loop', 'code'];
+console.log(`************ 4 **************`);
 
-// // TODO Use for of loop to fing longest word
+let words = ['pen', 'javascript', 'loop', 'code'];
 
-// let wordLength = [];
+// TODO Use for of loop to fing longest word
 
-// for (let word of words) {
-// }
+let wordLength = [];
 
-// console.log(wordLength);
+for (let word of words) {
+  // Find the length of each element
+  console.log(word.length);
+  // Find the longest element in the array
+
+  wordLength = words.reduce((acc, curr) =>
+    acc.length > curr.length ? acc : curr
+  );
+}
+
+console.log(wordLength);
+
+// ============================================
+// 5
+//  Problem:Reverse a string
+//  Date: 2025-08-24
+//  Language: JavaScript
+//  Difficulty: Medium
+// ============================================
+
+console.log(`************ 5  **************`);
+
+let text = 'This is crazy';
+
+// TODO Use a for of loop to reverse it "olleh"
+let reverse;
+for (let str of text) {
+  reverse = text.split('').reverse(' ').join('');
+}
+console.log(reverse);
+
+// ============================================
+// 6
+//  Problem:Filter even numbers
+//  Date: 2025-08-24
+//  Language: JavaScript
+//  Difficulty: Medium
+// ============================================
+
+console.log(`************ 6 **************`);
+
+let numbers2 = [1, 2, 3, 4, 5, 6];
+
+// TODO Use for of loop to create a new array of even numbers
+
+let evenNumbers = [];
+for (let even of numbers2) {
+  evenNumbers = numbers2.filter((even) => even % 2 == 0);
+}
+console.log(evenNumbers);
+
+// ============================================
+// 7
+//  Problem:Flatten nested arrays(level 1 deep)
+//  Date: 2025-08-24
+//  Language: JavaScript
+//  Difficulty: Hard
+// ============================================
+
+console.log(`************ 7 **************`);
+
+let arr = [[1, 2], [3, 4], [5]];
+
+// TODO Use for of loop to flatten -> [1,2,3,4,5]
+
+let flatArray = [];
+for (let flat of arr) {
+  flatArray = arr.flat();
+}
+console.log(flatArray);
