4b64191c4e8891d7cd0f5d24e1e9338939903598
Silviu <silviu_miron_90@outlook.com>
2025-08-26 11:02:06 +0100

    Solve enhanced_objects_literals assignment.

--- August_2025/completeJsCourseAugust/assignments.js
+++ August_2025/completeJsCourseAugust/assignments.js
@@ -542,3 +542,45 @@ function getCount(str) {
 }
 
 console.log(getCount('Hello World'));
+
+// ASSIGNMENT
+// Enhanced object literals
+// ============================================
+//
+//  Problem: Enhanced Object literals:
+//  Date: 2025-08-26
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+//9.1
+
+// Below the boodData array that contains other arrays.Each inner array considts of the property name (first element), and the value(second element).For example, in ["title","Computer Netwoking: A Top-Down Approch"],'title' is the property name, 'Computer Networking: A Top-Down Approch'is mean to be the value assigned to that property name.
+
+// Using computed properies, file the newBook object with the propoerties and values from the bookData array. The first one is done alreaday.
+
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+// 9.2
+
+// Below is the pags variables. Add it as property of the newBook2 object .Use the shorter way.
+
+const pages = 800;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kerighan', 'Denis M. Rithche'],
+  pages,
+};
+
+console.log(newBook2);
