06e5129750a2c5c213c336a2ba04ffd67e3b6be7
Silviu <silviu_miron_90@outlook.com>
2025-08-30 12:52:09 +0100

    Solve objec.keys,values,and ent challenges.

--- August_2025/completeJsCourseAugust/object-value-key-entries.js
+++ August_2025/completeJsCourseAugust/object-value-key-entries.js
@@ -159,6 +159,136 @@ console.log(Object.entries(movie));
 
 // 4 Loop through the entries and print sentence like: `The key 'title'has value'Inception'`
 
+console.log(`************************** 8 **********************`);
+
 for (const [title, value] of Object.entries(movie)) {
   console.log(`The key ${title} has value ${value}`);
 }
+
+// ============================================
+// 8
+//  Problem: Add new key-value Pair
+//  Date: 2025-08-30
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+const user2 = {
+  name: 'Emma',
+  age: 28,
+};
+
+// TODO Add a new key city with value london using dot notation
+// Then print the updated object
+
+console.log(`************************** 9 **********************`);
+
+user2.city = 'London';
+console.log(user2);
+console.log(Object.keys(user2.city));
+
+// ============================================
+// 9
+//  Problem: Update a Value
+//  Date: 2025-08-30
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+const laptop = {
+  brand: 'HP',
+  price: 700,
+};
+
+// TODO Update the price to 750 using brackte notation
+// Then print the update object
+console.log(`************************** 10 **********************`);
+laptop['price'] = 750;
+console.log(Object.values(laptop));
+
+// ============================================
+// 10
+//  Problem: Delete key
+//  Date: 2025-08-30
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+const phone = {
+  brand: 'Samsung',
+  model: 'S21',
+  color: 'Black',
+};
+
+// TODO Delete the color key from the object
+// Then print the upadated object
+
+delete phone.color;
+console.log(Object.keys(phone));
+
+// ============================================
+// 11
+//  Problem: Check if a Key Exists
+//  Date: 2025-08-30
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+let fruits = {
+  name: 'Apple',
+  color: 'Red',
+  price: 2,
+};
+
+// TODO Check if the key  color exist in the object
+// Print true of false
+
+console.log(`************************** 11 **********************`);
+
+if (Object.keys((fruits = 'color'))) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// ============================================
+// 12
+//  Problem: Check if a Key Exists
+//  Date: 2025-08-30
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+const city = {
+  name: 'Tokyo',
+  country: 'Japan',
+  population: 14000000,
+};
+
+// TODO Print how many keys and values the object has
+
+console.log(`************************** 12 **********************`);
+for (const [key, value] of Object.entries(city)) {
+  console.log(key, value);
+}
+
+// ============================================
+// 13
+//  Problem: Combine Keys and Values into String
+//  Date: 2025-08-30
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+const student2 = {
+  name: 'Liam',
+  grade: 'B',
+  subject: 'Science',
+};
+
+// TODO Loop throught the object and print sentences like:
+// "Liam studies Science and got grade B"
+
+console.log(
+  `${student2.name} studies ${student2.subject} and got grade ${student2.grade}.`
+);
