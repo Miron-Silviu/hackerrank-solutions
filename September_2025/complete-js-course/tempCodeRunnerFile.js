bfa5f8b40d089363199c73c695802e08375dded0
Silviu <silviu_miron_90@outlook.com>
2025-09-09 23:48:58 +0100

    Solving map challenges

--- September_2025/complete-js-course/tempCodeRunnerFile.js
+++ September_2025/complete-js-course/tempCodeRunnerFile.js
@@ -1,1 +1,9 @@
-duplicate
+const car = new Map([
+//   [1, 'Land Rover'],
+//   [2, 'Mercedez Benz'],
+//   [3, 'Dacia Duster'],
+//   [4, 'Renault'],
+// ]);
+
+// car.delete(1);
+// console.log(car);
