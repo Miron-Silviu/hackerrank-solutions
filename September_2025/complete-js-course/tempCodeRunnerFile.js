7867fc7caff0c9b4cec21b4e8f43752b20500a09
Silviu <silviu_miron_90@outlook.com>
2025-09-22 12:00:38 +0100

    Solving assignment string part 3

--- September_2025/complete-js-course/tempCodeRunnerFile.js
+++ September_2025/complete-js-course/tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-const str = 'Hello [World]';
-
-// const removeFirst = str.indexOf('[');
-// const removeSecond = str.indexOf(']');
-
-// const getString = str.slice(removeFirst + 1, removeSecond);
-
-// console.log(removeFirst);
-// console.log(removeSecond);
-// console.log(getString);
+bookChapters
