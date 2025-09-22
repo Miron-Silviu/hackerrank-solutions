310f740d5c614030c92a159e8e4079181a4ea48f
Silviu <silviu_miron_90@outlook.com>
2025-09-22 14:45:09 +0100

    Feat : Solving string methods

--- September_2025/complete-js-course/strings.js
+++ September_2025/complete-js-course/strings.js
@@ -133,3 +133,155 @@ function removeEverghing(str) {
 }
 
 console.log(removeEverghing('username@example.com'));
+
+// ============================================
+// 8
+//  Problem:Length of a string
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Write a program that asks user for thier name and prints how many characters it has.
+
+function nameLenght(name) {
+  return name.length;
+}
+console.log(nameLenght('Silviu Miron'));
+
+// ============================================
+// 9
+//  Problem:Upper and lowercase
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Convert "hello world" into all uppercase, all lowercase
+
+console.log('hello world'.toUpperCase());
+console.log('hello world'.toLowerCase());
+
+// ============================================
+// 10
+//  Problem:Find Position of a word
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO  Find the position(index) of the word "JavaScript"
+
+let sentence = 'I love JavaScript progamming';
+
+console.log(sentence.indexOf('JavaScript'));
+
+// ============================================
+// 11
+//  Problem:Extract part of a string
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Extract part of a string from "I love coding" extract only the word "coding"
+
+const coding = 'I love coding in JavaScript';
+
+console.log(coding.slice(7, 13));
+console.log(coding.substring(7, 13));
+
+// ============================================
+// 12
+//  Problem:Replace a word
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Change "I like python" to "I like JavaScript"
+
+const programmingLanguage = 'I like Python';
+
+console.log(
+  programmingLanguage.replace(programmingLanguage, 'I like JavaScript')
+);
+
+// ============================================
+// 13
+//  Problem:Check if word exists
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Write a program that check it"apple" esist in this sentence
+
+let text = 'I ate a red apple yesterday.';
+
+console.log(text.includes('apple'));
+
+// ============================================
+// 14
+//  Problem:Split into Words
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Turn this string into array.
+
+const string = 'Learn JavaScript step by step';
+
+console.log(string.split());
+
+// ============================================
+// 15
+//  Problem:Remove extra spaces
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Remove extra spaces
+
+const removSpaces = '  JavaScript is fun!         ';
+
+console.log(removSpaces.trim());
+
+// ============================================
+// 16
+//  Problem:Repeat string
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Pring "hi" 5 times
+
+const repetaString = 'hi';
+console.log(repetaString.repeat(5));
+
+// ============================================
+// 17
+//  Problem:Combine Methods
+//  Date: 2025-08-22
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Given " hello WORLD"
+// -remove spaces
+// -make it lowercase
+// -then make the first letter uppercase
+
+const str = ' hello WORLD';
+
+const combineMethods = function (methods) {
+  const newStr = str.trim().toLowerCase();
+
+  const firstUpper = newStr.slice(0, 1).toUpperCase() + newStr.slice(1);
+  console.log(firstUpper);
+};
+
+combineMethods();
