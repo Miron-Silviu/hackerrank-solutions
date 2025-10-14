83cc5b70c8a12b5dcb78519f7aa519f1686cde73
Silviu <silviu_miron_90@outlook.com>
2025-10-14 09:44:51 +0100

    Solve value and reference cahllenges.

--- October_2025/complete-js-course/tempCodeRunnerFile.js
+++ October_2025/complete-js-course/tempCodeRunnerFile.js
@@ -1,2 +1,8 @@
-const myObj = { name: 'Alice' };
-addProperty(myObj);
+function reassignObject(obj) {
+//   obj = { name: 'Bob' };
+//   console.log('Inside function', obj);
+// }
+
+// const originalObj = { name: 'Alice' };
+// reassignObject(originalObj);
+// console.log('Outside function: ', originalObj);
