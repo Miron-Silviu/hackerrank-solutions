ccdaba76a0026c5ad516a47829216376ca70fe1a
Silviu <silviu_miron_90@outlook.com>
2025-10-14 09:44:51 +0100

    Solve value and reference cahllenges.

--- October_2025/complete-js-course/value-and-reference.js
+++ October_2025/complete-js-course/value-and-reference.js
@@ -111,6 +111,7 @@ console.log(`Outside function:`, nestedObj);
 //  Difficulty: Easy
 // ============================================
 
+console.log(`********************** 5 ***********************`);
 function safeModify(obj) {
   const newObj = { ...obj };
   newObj.newProp = 'Safe';
@@ -123,3 +124,117 @@ const updateUser = safeModify(user);
 
 console.log(`Original :`, user);
 console.log(`Updated:`, updateUser);
+
+// ============================================
+// 6
+//  Problem: testing each concept
+//  Date: 2025-10-08
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+console.log(`********************** 6 ***********************`);
+
+function myFunction(param) {
+  param.username = 'Brian';
+  console.log(`Inside Function:`, param);
+}
+
+const data = { name: 'Bob' };
+myFunction(data);
+
+console.log(`Outside Function :`, data);
+
+// TODO Predint the output
+
+// ============================================
+// 7
+//  Problem: testing each concept
+//  Date: 2025-10-08
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+console.log(`********************** 7 ***********************`);
+const testing = function (par) {
+  par = { name: 'Alan', age: 30 };
+  console.log(`Inside Function:`, par);
+};
+
+const newData = { name: 'Alice', age: 35 };
+testing(newData);
+console.log(`Outside Function:`, newData);
+
+// ============================================
+// 8
+//  Problem: nested concept
+//  Date: 2025-10-08
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+console.log(`********************** 8 ***********************`);
+const nestedProperty = function (param) {
+  param.obj = { name: 'Username' };
+  console.log(`Inside Function:`, param);
+};
+
+const nested = { inside: { name: 'name' } };
+
+nestedProperty(nested);
+console.log(`Outside Function:`, nested);
+
+// ============================================
+// 9
+//  Problem: copying object inside function
+//  Date: 2025-10-08
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+console.log(`********************** 9 ***********************`);
+
+const copyingOBject = function (param) {
+  const copying = { ...param };
+  console.log(`Inside Function:`, copying);
+};
+
+const username = { firstName: 'Alan', secondName: 'Turning' };
+copyingOBject(username);
+console.log(`Outside Function:`, username);
+
+// ============================================
+// 10
+//  Problem: base
+//  Date: 2025-10-08
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+console.log(`********************** 9 ***********************`);
+function testFunc(param) {
+  param = { changed: true };
+  console.log(`Inside Function`, param);
+}
+
+const data2 = { pesonalNumber: 10 };
+testFunc(data2);
+console.log(`Outside Function:`, data2);
+
+console.log(`********************** 10 ***********************`);
+
+const testingAll = function (param) {
+  param.number = true;
+  console.log(`Mutate `, param);
+
+  param = { changed: true };
+
+  console.log(`Reasign:`, param);
+
+  const copy = { ...param };
+  copy.changed = true;
+  console.log(`Copy`, copy);
+};
+
+const testingObj = { 'lucky number': 10 };
+testingAll(testingObj);
+console.log(`Outside Function:`, testingObj);
