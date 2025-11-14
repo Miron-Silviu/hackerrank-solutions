56c2712c3cf92d22083108c390cc9135bbd16d8e
Miron Silviu <silviu_miron_90@outlook.com>
2025-11-14 21:45:26 +0000

    Solve challenge using slice method.

--- november_2025/complete_js_course/array_methods_part1.js
+++ november_2025/complete_js_course/array_methods_part1.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// ============================================
+// 1
+//  Problem:Slice ()
+//  Date: 2025-11-14
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Given the fruit array, extract banana and cherry using slice.
+
+const fruits = ['apple', 'banana', 'charry', 'date', 'elderberry'];
+console.log(fruits.slice(1, 3));
+
+// ============================================
+// 2
+//  Problem:Slice ()
+//  Date: 2025-11-14
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
