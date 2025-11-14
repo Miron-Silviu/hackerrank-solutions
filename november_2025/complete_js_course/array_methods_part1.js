0be2db4425a54eb840f3ba7b4aa0a583694086e0
Miron Silviu <silviu_miron_90@outlook.com>
2025-11-14 22:01:50 +0000

    Create new array using slice method.

--- november_2025/complete_js_course/array_methods_part1.js
+++ november_2025/complete_js_course/array_methods_part1.js
@@ -10,6 +10,8 @@
 
 // TODO Given the fruit array, extract banana and cherry using slice.
 
+console.log(`******************************* 1 ************************`);
+
 const fruits = ['apple', 'banana', 'charry', 'date', 'elderberry'];
 console.log(fruits.slice(1, 3));
 
@@ -23,6 +25,20 @@ console.log(fruits.slice(1, 3));
 
 // TODO Use slice to copy the last 2 elements
 
-const nums = [10, 20, 30, 40, 50];
+console.log(`******************************* 2 ************************`);
 
+const nums = [10, 20, 30, 40, 50];
 console.log(nums.slice(3, nums.length));
+
+// ============================================
+// 3
+//  Problem:Slice ()
+//  Date: 2025-11-14
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Use slice to create a new array
+
+const colors = ['red', 'green', 'blue', 'yellow'];
+console.log(colors.slice(1, colors.length));
