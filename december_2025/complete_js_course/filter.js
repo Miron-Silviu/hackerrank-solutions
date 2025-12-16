169a4b0b30f7a95d94b6650ad7d24ccadfd115fe
Miron Silviu <silviu_miron_90@outlook.com>
2025-12-16 11:26:37 +0000

    Challenge two find words longer than 5

--- december_2025/complete_js_course/filter.js
+++ december_2025/complete_js_course/filter.js
@@ -1,5 +1,6 @@
 'use strict';
 
+console.log(`------------------ First Challenge -------------------`);
 // ============================================
 // 1
 //  Problem:Filter
@@ -14,3 +15,19 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const evens = numbers.filter((x) => x % 2 === 0);
 console.log(evens);
+
+console.log(`------------------ Second Chllenge -------------------`);
+// ============================================
+// 2
+//  Problem:Filter long words
+//  Date: 2025-11-14
+//  Language: JavaScript
+//  Difficulty: Easy
+// ============================================
+
+// TODO Use filter to create a new array that combing only words longers than 5 characters
+
+const words = ['apple', 'banana', 'kiwi', 'grape', 'watermelon'];
+
+const longerThanFive = words.filter((x) => x.length > 5);
+console.log(longerThanFive);
